80cf8aac0709e42dd473463a6b1693ecd97f09ce
slhmo <mohamadzadehsaleh7@gmail.com>
2024-11-15 17:36:01 +0330

    made it 2d

--- files/main.py
+++ files/main.py
@@ -1,30 +1,44 @@
 arr = [
-    ['xoooxxooo'], ['xoooxxooo'], ['xoooxxooo'], ['xoooxxooo'], ['xoxoxxooo'],
+    'xoooxxooox', 'xooooooooo', 'xooxxxooox', 'xoooxxoxox',
 ]
 
-
-def ship_counter(a_list):
-    divided = []
-    ships = 0
-    for i in a_list:
-        for j in str(*i):
-            divided.append(j)
+ships = 0
 
 
-    for i in range(len(divided)-1):
-        try:
-            if str(divided[i]) == (divided[i+1]):
-                divided.pop(i)
+for i in range(len(arr)):
+    print(arr[i])
+    # arr[i] == 'xoooxxooox'
+    # arr [i][j] == 'x'
 
-        except IndexError:
-            pass
+    for j in range(len(arr[i])):
+        #    i = rows     , j = columns
+        #    i = [0, 3]   , j = [0, 9]
+
+        if arr[i][j] == 'x':
+
+            if i == (len(arr)-1) and j == (len(arr[0])-1):  # i and j max
+                ships +=1
+
+            elif i == (len(arr)-1):   # i max (last row)
+                if arr[i][j+1] == 'x':
+                    pass
+                elif arr[i][j+1] != 'x':
+                    ships +=1
+
+            elif j == (len(arr[0])-1):   # j max (last column)
+                if arr[i+1][j] == 'x':
+                    pass
+                elif arr[i+1][j] != 'x':
+                    ships += 1
+
+            elif i != (len(arr)-1) and j != (len(arr[0])-1):
+                if arr[i][j+1] == 'x' or arr[i+1][j] == 'x':
+                    pass
 
 
-    for i in divided:
-        if i == 'x':
-            ships += 1
-    return ships
+                elif arr[i][j+1] != 'x' and arr[i+1][j] != 'x':
+                    ships += 1
 
 
 
-print(ship_counter(arr))
+print(ships)
