c047d34ca4bc318b1c556d1a2401e42ed3fb2ab5
slhmo <mohamadzadehsaleh7@gmail.com>
2024-11-15 19:03:46 +0330

    added user input feature

--- files/main.py
+++ files/main.py
@@ -1,42 +1,72 @@
-arr = [
-    'xoooxxooox', 'xooooooooo', 'xooxxxooox', 'xoooxxoxox',
-]
+try:
+    rows, columns = map(int, input('enter rows and columns').split())
+except ValueError:
+    print('wrong input try again.')
+except NameError:
+    print('wrong input try again.')
 
 ships = 0
+game_plain = []
+
+def get_game_plain():
+    row, column = 0, 0
+
+    while row in range(rows):
+        game_plain.append('')
+
+        while column in range(columns):
+            value = (input(': '))
+
+            if value == 'x' or value == 'o':
+                game_plain[row] += value
+                column +=1
+
+            else:
+                print('wrong input, try again')
+
+        row +=1
+        column = 0
 
 
-for i in range(len(arr)):
-    print(arr[i])
+get_game_plain()
+
+
+for i in range(len(game_plain)):
+    print(game_plain[i])
     # arr[i] == 'xoooxxooox'
     # arr [i][j] == 'x'
 
-    for j in range(len(arr[i])):
+    for j in range(len(game_plain[i])):
         #    i = rows     , j = columns
         #    i = [0, 3]   , j = [0, 9]
 
-        if arr[i][j] == 'x':
+        if game_plain[i][j] == 'x':
 
-            if i == (len(arr)-1) and j == (len(arr[0])-1):  # i and j max
+            if i == (len(game_plain)-1) and j == (len(game_plain[0])-1):  # i and j max
                 ships +=1
 
-            elif i == (len(arr)-1):   # i max (last row)
-                if arr[i][j+1] == 'x':
+            elif i == (len(game_plain)-1):   # i max (last row)
+                if game_plain[i][j+1] == 'x':
                     pass
-                elif arr[i][j+1] != 'x':
+                elif game_plain[i][j+1] != 'x':
                     ships +=1
 
-            elif j == (len(arr[0])-1):   # j max (last column)
-                if arr[i+1][j] == 'x':
+            elif j == (len(game_plain[0])-1):   # j max (last column)
+                if game_plain[i+1][j] == 'x':
                     pass
-                elif arr[i+1][j] != 'x':
+                elif game_plain[i+1][j] != 'x':
                     ships += 1
 
-            elif i != (len(arr)-1) and j != (len(arr[0])-1):
-                if arr[i][j+1] == 'x' or arr[i+1][j] == 'x':
+            elif i != (len(game_plain)-1) and j != (len(game_plain[0])-1):
+                if game_plain[i][j+1] == 'x' and game_plain[i+1][j] == 'x':
+                    print('wrong input.'
+                          ' a ship can only be vertical or horizontal.')
+
+                if game_plain[i][j+1] == 'x' or game_plain[i+1][j] == 'x':
                     pass
 
 
-                elif arr[i][j+1] != 'x' and arr[i+1][j] != 'x':
+                elif game_plain[i][j+1] != 'x' and game_plain[i+1][j] != 'x':
                     ships += 1
 
 
